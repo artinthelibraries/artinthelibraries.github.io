0a1258825bcf195bb907edb17dac9227271fc517
agustinurioste <76100677+agustinurioste@users.noreply.github.com>
2021-02-23 22:30:59 -0500

    Update main.js

--- apple/main.js
+++ apple/main.js
@@ -5,7 +5,7 @@ const context = canvas.getContext("2d");
 const frameCount = 342;
 const currentFrame = index => (
  //`https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
- `https://raw.githubusercontent.com/agustinurioste/agustinurioste.github.io/main/assets/oop/${index.toString().padStart(5, '0')}.jpg`
+ `https://raw.githubusercontent.com/artinthelibraries/wvwomen/main/assets/oop/${index.toString().padStart(5, '0')}.jpg`
  )
 
 const preloadImages = () => {
